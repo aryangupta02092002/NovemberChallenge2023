87513be3a4d8d901852fb2e8cf7c4f70d84580dd
Aryan Gupta <72204017+aryangupta02092002@users.noreply.github.com>
2023-11-12 14:21:20 +0530

    Nov 12 - String | Reversal

---
+++ GFG/CheckIfStringIsRotatedByTwoPlaces.cpp
@@ -0,0 +1,51 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution
+{
+    public:
+    //Function to check if a string can be obtained by rotating
+    //another string by exactly 2 places.
+    bool helper(string s, string t){
+        int i = 0;
+        while(i < s.size()){
+            if(s[i] != t[(i+2) % s.size()]){
+                return false;
+            }
+            i++;
+        }
+        return true;
+    }
+    bool isRotated(string str1, string str2)
+    {
+        // Your code here
+        if(str1.size() != str2.size()){
+            return false;
+        }
+        return helper(str1, str2) || helper(str2, str1);
+    }
+
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+	
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		string s;
+		string b;
+		cin>>s>>b;
+		Solution obj;
+		cout<<obj.isRotated(s,b)<<endl;
+	}
+	return 0;
+}
+
+// } Driver Code Ends
