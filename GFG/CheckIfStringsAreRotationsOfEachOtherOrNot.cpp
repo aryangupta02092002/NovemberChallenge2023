84df753c7f477757e2099d520821ffb6c6ab5b1b
Aryan Gupta <72204017+aryangupta02092002@users.noreply.github.com>
2023-11-14 10:52:21 +0530

    Nov 14 - Strings | Rotational

---
+++ GFG/CheckIfStringsAreRotationsOfEachOtherOrNot.cpp
@@ -0,0 +1,46 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution
+{
+    public:
+    //Function to check if two strings are rotations of each other or not.
+    bool areRotations(string s1,string s2)
+    {
+        // Your code here
+        string s3 = s1 + s1;
+        int sz1 = s3.size(), sz2 = s2.size();
+        
+        for(int i=0; i<=sz1-sz2; i++){
+            string s = s3.substr(i, sz2);
+            if(s == s2){
+                return true;
+            }
+        }
+        return false;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        string s1;
+        string s2;
+        cin>>s1>>s2;
+        Solution obj;
+        cout<<obj.areRotations(s1,s2)<<endl;
+
+    }
+    return 0;
+}
+
+// } Driver Code Ends
